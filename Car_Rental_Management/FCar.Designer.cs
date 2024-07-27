f59966e65299019d6bb31796dd0183f54178ec6c
Tuan <nguyenhahongtuan@gmail.com>
2024-07-27 23:59:06 +0700

    Create some button...

---
+++ Car_Rental_Management/FCar.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Car_Rental_Management
+{
+    partial class FCar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Search = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.button_Search = new Guna.UI2.WinForms.Guna2Button();
+            this.comboBox_Transmission = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.comboBox_FuelType = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.button_Add = new Guna.UI2.WinForms.Guna2Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // textBox_Search
+            // 
+            this.textBox_Search.AutoRoundedCorners = true;
+            this.textBox_Search.BorderRadius = 29;
+            this.textBox_Search.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox_Search.DefaultText = "";
+            this.textBox_Search.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.textBox_Search.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.textBox_Search.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textBox_Search.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textBox_Search.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textBox_Search.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.textBox_Search.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textBox_Search.Location = new System.Drawing.Point(14, 15);
+            this.textBox_Search.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox_Search.Name = "textBox_Search";
+            this.textBox_Search.PasswordChar = '\0';
+            this.textBox_Search.PlaceholderText = "Search...";
+            this.textBox_Search.SelectedText = "";
+            this.textBox_Search.Size = new System.Drawing.Size(450, 60);
+            this.textBox_Search.TabIndex = 0;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.AutoRoundedCorners = true;
+            this.guna2Button2.BackColor = System.Drawing.SystemColors.Control;
+            this.guna2Button2.BorderRadius = 29;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.White;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.guna2Button2.Image = global::Car_Rental_Management.Properties.Resources.icons8_refresh_48;
+            this.guna2Button2.Location = new System.Drawing.Point(558, 15);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(80, 60);
+            this.guna2Button2.TabIndex = 2;
+            // 
+            // button_Search
+            // 
+            this.button_Search.AutoRoundedCorners = true;
+            this.button_Search.BackColor = System.Drawing.SystemColors.Control;
+            this.button_Search.BorderRadius = 29;
+            this.button_Search.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.button_Search.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.button_Search.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.button_Search.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.button_Search.FillColor = System.Drawing.Color.White;
+            this.button_Search.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.button_Search.ForeColor = System.Drawing.SystemColors.Control;
+            this.button_Search.Image = global::Car_Rental_Management.Properties.Resources.icons8_search_48;
+            this.button_Search.Location = new System.Drawing.Point(472, 15);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(80, 60);
+            this.button_Search.TabIndex = 1;
+            // 
+            // comboBox_Transmission
+            // 
+            this.comboBox_Transmission.AutoRoundedCorners = true;
+            this.comboBox_Transmission.BackColor = System.Drawing.Color.Transparent;
+            this.comboBox_Transmission.BorderRadius = 17;
+            this.comboBox_Transmission.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox_Transmission.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Transmission.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox_Transmission.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox_Transmission.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.comboBox_Transmission.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.comboBox_Transmission.ItemHeight = 30;
+            this.comboBox_Transmission.Items.AddRange(new object[] {
+            "Transmission (All)"});
+            this.comboBox_Transmission.Location = new System.Drawing.Point(644, 39);
+            this.comboBox_Transmission.Name = "comboBox_Transmission";
+            this.comboBox_Transmission.Size = new System.Drawing.Size(246, 36);
+            this.comboBox_Transmission.StartIndex = 0;
+            this.comboBox_Transmission.TabIndex = 3;
+            // 
+            // comboBox_FuelType
+            // 
+            this.comboBox_FuelType.AutoRoundedCorners = true;
+            this.comboBox_FuelType.BackColor = System.Drawing.Color.Transparent;
+            this.comboBox_FuelType.BorderRadius = 17;
+            this.comboBox_FuelType.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox_FuelType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_FuelType.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox_FuelType.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox_FuelType.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.comboBox_FuelType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.comboBox_FuelType.ItemHeight = 30;
+            this.comboBox_FuelType.Items.AddRange(new object[] {
+            "FuelType (All)",
+            "Gas",
+            "Diesel Oil",
+            "Electricity"});
+            this.comboBox_FuelType.Location = new System.Drawing.Point(896, 39);
+            this.comboBox_FuelType.Name = "comboBox_FuelType";
+            this.comboBox_FuelType.Size = new System.Drawing.Size(218, 36);
+            this.comboBox_FuelType.StartIndex = 0;
+            this.comboBox_FuelType.TabIndex = 4;
+            // 
+            // button_Add
+            // 
+            this.button_Add.AutoRoundedCorners = true;
+            this.button_Add.BorderRadius = 21;
+            this.button_Add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.button_Add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.button_Add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.button_Add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.button_Add.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Add.ForeColor = System.Drawing.Color.White;
+            this.button_Add.Location = new System.Drawing.Point(1545, 30);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(180, 45);
+            this.button_Add.TabIndex = 5;
+            this.button_Add.Text = "ADD";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tableLayoutPanel1.ColumnCount = 9;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.Color.Black;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 84);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1711, 100);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // FCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1737, 657);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.comboBox_FuelType);
+            this.Controls.Add(this.comboBox_Transmission);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.textBox_Search);
+            this.Name = "FCar";
+            this.Text = "FCar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox textBox_Search;
+        private Guna.UI2.WinForms.Guna2Button button_Search;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2ComboBox comboBox_Transmission;
+        private Guna.UI2.WinForms.Guna2ComboBox comboBox_FuelType;
+        private Guna.UI2.WinForms.Guna2Button button_Add;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
--- Car_Rental_Management/Properties/Resources.Designer.cs
+++ Car_Rental_Management/Properties/Resources.Designer.cs
@@ -79,5 +79,25 @@ namespace Car_Rental_Management.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_refresh_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-refresh-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_search_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-search-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
