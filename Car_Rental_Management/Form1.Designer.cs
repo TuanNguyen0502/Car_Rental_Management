4646f6de7dda01a827941775be3c0d197dfebb0c
Duyen <thihoangduyendo@gmail.com>
2024-07-28 10:13:05 +0700

    Add client form

--- Car_Rental_Management/Form1.Designer.cs
+++ Car_Rental_Management/Form1.Designer.cs
@@ -29,27 +29,21 @@
         private void InitializeComponent()
         {
             this.pnl_Content = new Guna.UI2.WinForms.Guna2Panel();
-            this.pnl_Container = new Guna.UI2.WinForms.Guna2Panel();
             this.btn_Contract = new Guna.UI2.WinForms.Guna2Button();
+            this.pnl_Container = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnClient = new Guna.UI2.WinForms.Guna2Button();
             this.pnl_Content.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl_Content
             // 
+            this.pnl_Content.Controls.Add(this.btnClient);
             this.pnl_Content.Controls.Add(this.btn_Contract);
             this.pnl_Content.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnl_Content.Location = new System.Drawing.Point(0, 0);
             this.pnl_Content.Name = "pnl_Content";
             this.pnl_Content.Size = new System.Drawing.Size(200, 660);
             this.pnl_Content.TabIndex = 0;
-            // 
-            // pnl_Container
-            // 
-            this.pnl_Container.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnl_Container.Location = new System.Drawing.Point(200, 0);
-            this.pnl_Container.Name = "pnl_Container";
-            this.pnl_Container.Size = new System.Drawing.Size(1140, 660);
-            this.pnl_Container.TabIndex = 1;
             // 
             // btn_Contract
             // 
@@ -59,12 +53,36 @@
             this.btn_Contract.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btn_Contract.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btn_Contract.ForeColor = System.Drawing.Color.White;
-            this.btn_Contract.Location = new System.Drawing.Point(14, 115);
+            this.btn_Contract.Location = new System.Drawing.Point(11, 115);
             this.btn_Contract.Name = "btn_Contract";
             this.btn_Contract.Size = new System.Drawing.Size(180, 45);
             this.btn_Contract.TabIndex = 0;
             this.btn_Contract.Text = "Hợp đồng";
             this.btn_Contract.Click += new System.EventHandler(this.btn_Contract_Click);
+            // 
+            // pnl_Container
+            // 
+            this.pnl_Container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_Container.Location = new System.Drawing.Point(200, 0);
+            this.pnl_Container.Name = "pnl_Container";
+            this.pnl_Container.Size = new System.Drawing.Size(1140, 660);
+            this.pnl_Container.TabIndex = 1;
+            // 
+            // btnClient
+            // 
+            this.btnClient.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnClient.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnClient.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnClient.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnClient.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClient.ForeColor = System.Drawing.Color.White;
+            this.btnClient.Image = global::Car_Rental_Management.Properties.Resources.client;
+            this.btnClient.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnClient.Location = new System.Drawing.Point(11, 194);
+            this.btnClient.Name = "btnClient";
+            this.btnClient.Size = new System.Drawing.Size(180, 45);
+            this.btnClient.TabIndex = 1;
+            this.btnClient.Text = "Khách hàng";
             // 
             // Form1
             // 
@@ -86,6 +104,7 @@
         private Guna.UI2.WinForms.Guna2Panel pnl_Content;
         private Guna.UI2.WinForms.Guna2Panel pnl_Container;
         private Guna.UI2.WinForms.Guna2Button btn_Contract;
+        private Guna.UI2.WinForms.Guna2Button btnClient;
     }
 }
 
--- Car_Rental_Management/Properties/Resources.Designer.cs
+++ Car_Rental_Management/Properties/Resources.Designer.cs
@@ -63,6 +63,36 @@ namespace Car_Rental_Management.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap client {
+            get {
+                object obj = ResourceManager.GetObject("client", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_add_30 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-add-30", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_delete_30 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-delete-30", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_find_16 {
             get {
                 object obj = ResourceManager.GetObject("icons8-find-16", resourceCulture);
