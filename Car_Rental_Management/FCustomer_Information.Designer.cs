05b0c7b960bbcc16287de80f3fed2fc078532157
Duyen <thihoangduyendo@gmail.com>
2024-07-29 16:01:23 +0700

    Add FCustomer Information

---
+++ Car_Rental_Management/FCustomer_Information.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Car_Rental_Management
+{
+    partial class FCustomer_Information
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_FullName = new System.Windows.Forms.Label();
+            this.txt_FullName = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lbl_Sex = new System.Windows.Forms.Label();
+            this.txt_CCCD = new Guna.UI2.WinForms.Guna2TextBox();
+            this.cbx_Sex = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.lbl_Birthday = new System.Windows.Forms.Label();
+            this.dtp_Birthday = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            this.lbl_CCCD = new System.Windows.Forms.Label();
+            this.lbl_Address = new System.Windows.Forms.Label();
+            this.txt_Address = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtPhone = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lbl_Phone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_FullName
+            // 
+            this.lbl_FullName.AutoSize = true;
+            this.lbl_FullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_FullName.Location = new System.Drawing.Point(28, 50);
+            this.lbl_FullName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_FullName.Name = "lbl_FullName";
+            this.lbl_FullName.Size = new System.Drawing.Size(85, 20);
+            this.lbl_FullName.TabIndex = 0;
+            this.lbl_FullName.Text = "Full Name";
+            // 
+            // txt_FullName
+            // 
+            this.txt_FullName.BorderColor = System.Drawing.Color.Blue;
+            this.txt_FullName.BorderRadius = 20;
+            this.txt_FullName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_FullName.DefaultText = "";
+            this.txt_FullName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_FullName.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_FullName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_FullName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_FullName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_FullName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_FullName.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_FullName.Location = new System.Drawing.Point(178, 38);
+            this.txt_FullName.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txt_FullName.Name = "txt_FullName";
+            this.txt_FullName.PasswordChar = '\0';
+            this.txt_FullName.PlaceholderText = "Nguyễn Văn A";
+            this.txt_FullName.SelectedText = "";
+            this.txt_FullName.Size = new System.Drawing.Size(264, 43);
+            this.txt_FullName.TabIndex = 1;
+            // 
+            // lbl_Sex
+            // 
+            this.lbl_Sex.AutoSize = true;
+            this.lbl_Sex.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Sex.Location = new System.Drawing.Point(28, 149);
+            this.lbl_Sex.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Sex.Name = "lbl_Sex";
+            this.lbl_Sex.Size = new System.Drawing.Size(37, 20);
+            this.lbl_Sex.TabIndex = 2;
+            this.lbl_Sex.Text = "Sex";
+            // 
+            // txt_CCCD
+            // 
+            this.txt_CCCD.BorderColor = System.Drawing.Color.Blue;
+            this.txt_CCCD.BorderRadius = 20;
+            this.txt_CCCD.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_CCCD.DefaultText = "";
+            this.txt_CCCD.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_CCCD.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_CCCD.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_CCCD.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_CCCD.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_CCCD.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_CCCD.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_CCCD.Location = new System.Drawing.Point(178, 330);
+            this.txt_CCCD.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txt_CCCD.Name = "txt_CCCD";
+            this.txt_CCCD.PasswordChar = '\0';
+            this.txt_CCCD.PlaceholderText = "0533632367";
+            this.txt_CCCD.SelectedText = "";
+            this.txt_CCCD.Size = new System.Drawing.Size(264, 43);
+            this.txt_CCCD.TabIndex = 3;
+            // 
+            // cbx_Sex
+            // 
+            this.cbx_Sex.BackColor = System.Drawing.Color.Transparent;
+            this.cbx_Sex.BorderColor = System.Drawing.Color.Blue;
+            this.cbx_Sex.BorderRadius = 20;
+            this.cbx_Sex.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbx_Sex.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_Sex.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbx_Sex.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbx_Sex.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbx_Sex.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cbx_Sex.ItemHeight = 30;
+            this.cbx_Sex.Items.AddRange(new object[] {
+            "Female",
+            "Male",
+            "Other"});
+            this.cbx_Sex.Location = new System.Drawing.Point(178, 133);
+            this.cbx_Sex.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbx_Sex.Name = "cbx_Sex";
+            this.cbx_Sex.Size = new System.Drawing.Size(264, 36);
+            this.cbx_Sex.StartIndex = 0;
+            this.cbx_Sex.TabIndex = 4;
+            // 
+            // lbl_Birthday
+            // 
+            this.lbl_Birthday.AutoSize = true;
+            this.lbl_Birthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Birthday.Location = new System.Drawing.Point(28, 243);
+            this.lbl_Birthday.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Birthday.Name = "lbl_Birthday";
+            this.lbl_Birthday.Size = new System.Drawing.Size(105, 20);
+            this.lbl_Birthday.TabIndex = 5;
+            this.lbl_Birthday.Text = "Date of Birth";
+            // 
+            // dtp_Birthday
+            // 
+            this.dtp_Birthday.Checked = true;
+            this.dtp_Birthday.FillColor = System.Drawing.Color.Transparent;
+            this.dtp_Birthday.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_Birthday.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            this.dtp_Birthday.Location = new System.Drawing.Point(178, 227);
+            this.dtp_Birthday.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtp_Birthday.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
+            this.dtp_Birthday.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
+            this.dtp_Birthday.Name = "dtp_Birthday";
+            this.dtp_Birthday.Size = new System.Drawing.Size(264, 45);
+            this.dtp_Birthday.TabIndex = 6;
+            this.dtp_Birthday.Value = new System.DateTime(2024, 7, 29, 15, 52, 9, 290);
+            // 
+            // lbl_CCCD
+            // 
+            this.lbl_CCCD.AutoSize = true;
+            this.lbl_CCCD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_CCCD.Location = new System.Drawing.Point(28, 341);
+            this.lbl_CCCD.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_CCCD.Name = "lbl_CCCD";
+            this.lbl_CCCD.Size = new System.Drawing.Size(58, 20);
+            this.lbl_CCCD.TabIndex = 7;
+            this.lbl_CCCD.Text = "CCCD";
+            // 
+            // lbl_Address
+            // 
+            this.lbl_Address.AutoSize = true;
+            this.lbl_Address.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Address.Location = new System.Drawing.Point(28, 430);
+            this.lbl_Address.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Address.Name = "lbl_Address";
+            this.lbl_Address.Size = new System.Drawing.Size(71, 20);
+            this.lbl_Address.TabIndex = 8;
+            this.lbl_Address.Text = "Address";
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.BorderColor = System.Drawing.Color.Blue;
+            this.txt_Address.BorderRadius = 20;
+            this.txt_Address.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_Address.DefaultText = "";
+            this.txt_Address.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_Address.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_Address.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_Address.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_Address.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_Address.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Address.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_Address.Location = new System.Drawing.Point(178, 413);
+            this.txt_Address.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.PasswordChar = '\0';
+            this.txt_Address.PlaceholderText = "1 Võ Văn Ngân, Thủ Đức";
+            this.txt_Address.SelectedText = "";
+            this.txt_Address.Size = new System.Drawing.Size(264, 43);
+            this.txt_Address.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BorderColor = System.Drawing.Color.Blue;
+            this.txtPhone.BorderRadius = 20;
+            this.txtPhone.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPhone.DefaultText = "";
+            this.txtPhone.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtPhone.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtPhone.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPhone.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPhone.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPhone.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPhone.Location = new System.Drawing.Point(178, 511);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.PasswordChar = '\0';
+            this.txtPhone.PlaceholderText = "1 Võ Văn Ngân, Thủ Đức";
+            this.txtPhone.SelectedText = "";
+            this.txtPhone.Size = new System.Drawing.Size(264, 43);
+            this.txtPhone.TabIndex = 11;
+            // 
+            // lbl_Phone
+            // 
+            this.lbl_Phone.AutoSize = true;
+            this.lbl_Phone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Phone.Location = new System.Drawing.Point(28, 528);
+            this.lbl_Phone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Phone.Name = "lbl_Phone";
+            this.lbl_Phone.Size = new System.Drawing.Size(56, 20);
+            this.lbl_Phone.TabIndex = 10;
+            this.lbl_Phone.Text = "Phone";
+            // 
+            // FCustomer_Information
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 639);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lbl_Phone);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.lbl_Address);
+            this.Controls.Add(this.lbl_CCCD);
+            this.Controls.Add(this.dtp_Birthday);
+            this.Controls.Add(this.lbl_Birthday);
+            this.Controls.Add(this.cbx_Sex);
+            this.Controls.Add(this.txt_CCCD);
+            this.Controls.Add(this.lbl_Sex);
+            this.Controls.Add(this.txt_FullName);
+            this.Controls.Add(this.lbl_FullName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FCustomer_Information";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FCustomer_Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_FullName;
+        private Guna.UI2.WinForms.Guna2TextBox txt_FullName;
+        private System.Windows.Forms.Label lbl_Sex;
+        private Guna.UI2.WinForms.Guna2TextBox txt_CCCD;
+        private Guna.UI2.WinForms.Guna2ComboBox cbx_Sex;
+        private System.Windows.Forms.Label lbl_Birthday;
+        private Guna.UI2.WinForms.Guna2DateTimePicker dtp_Birthday;
+        private System.Windows.Forms.Label lbl_CCCD;
+        private System.Windows.Forms.Label lbl_Address;
+        private Guna.UI2.WinForms.Guna2TextBox txt_Address;
+        private Guna.UI2.WinForms.Guna2TextBox txtPhone;
+        private System.Windows.Forms.Label lbl_Phone;
+    }
+}
