8a5a219523bedb3e7ec12d9d43b86cd91d65cef4
Duyen <thihoangduyendo@gmail.com>
2024-07-30 19:37:08 +0700

    Change Program.cs

--- Car_Rental_Management/Program.cs
+++ Car_Rental_Management/Program.cs
@@ -18,7 +18,7 @@ namespace Car_Rental_Management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FCustomer_Information());
+            Application.Run(new FMain());
         }
     }
 }
