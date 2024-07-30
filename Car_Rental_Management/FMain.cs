ba90d3ebc1902a9725599c34d48b89c13e073d9f
Tuan <nguyenhahongtuan@gmail.com>
2024-07-30 17:34:34 +0700

    Sample code to create data

--- Car_Rental_Management/FMain.cs
+++ Car_Rental_Management/FMain.cs
@@ -13,10 +13,22 @@ namespace Car_Rental_Management
 {
     public partial class FMain : Form
     {
+        // List of data
+        private List<Contract> contracts = new List<Contract>();
+        private List<Customer> customers = new List<Customer>();
+        private List<Employee> employees = new List<Employee>();
+        private List<Ford> fords = new List<Ford>();
+        private List<Mazda> mazdas = new List<Mazda>();
+        private List<Mercedes> mercedes = new List<Mercedes>();
+        private List<VinFast> vinFasts = new List<VinFast>();
+
+        // Current form
         Form currentFormChild;
+
         public FMain()
         {
             InitializeComponent();
+            InitializeData();
         }
 
         private void OpenChildForm(Form childForm, Panel panel_Contains)
@@ -44,7 +56,19 @@ namespace Car_Rental_Management
 
         private void btnClient_Click(object sender, EventArgs e)
         {
+
+        }
+        private void InitializeData()
+        {
+
         }
 
+        private void InitializeEmployees()
+        {
+            DateTime Tuan_dob = new DateTime(2004, 02, 05);
+            DateTime Tuan_startDate = new DateTime(2021, 07, 01);
+            Employee Tuan = new Employee("Nguyen Ha Hong Tuan", Tuan_dob, "123456789", "Binh Duong", "0705488458", "nguyenhahongtuan@gmail.com",
+                "Nam", "EID001", "5M $", Tuan_startDate);
+        }
     }
 }
